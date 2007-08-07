a9395676a9f8209b497706f5e4c6f0af636ece5d
chadfowler <chadfowler@06148572-b36b-44fe-9aa8-f68b04d8b080>
2007-08-07 21:47:29 +0000

    git-svn-id: svn+ssh://rubyforge.org/var/svn/facebooker/trunk/facebooker@28 06148572-b36b-44fe-9aa8-f68b04d8b080

--- lib/facebooker/notifications.rb
+++ lib/facebooker/notifications.rb
@@ -4,7 +4,7 @@ module Facebooker
     attr_accessor :messages, :group_invites, :pokes, :friend_requests, :event_invites, :shares
     
     [:Messages, :Pokes, :Shares].each do |notification_type|
-      Notifications.const_set(notification_type, Class.new do
+      const_set(notification_type, Class.new do
         include Model
         attr_accessor :unread, :most_recent
       end)
