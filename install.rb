d9a7769db26aad3fe33c66f94e238631f427c43f
mmangino <mmangino@06148572-b36b-44fe-9aa8-f68b04d8b080>
2008-03-14 20:11:31 +0000

    Renamed js
    git-svn-id: svn+ssh://rubyforge.org/var/svn/facebooker/trunk/facebooker@204 06148572-b36b-44fe-9aa8-f68b04d8b080

--- install.rb
+++ install.rb
@@ -2,7 +2,7 @@ require 'fileutils'
 require 'rubygems'
 require 'ruby-debug'
 facebook_config = File.join(RAILS_ROOT,"config","facebooker.yml")
-facebook_js = File.join(RAILS_ROOT,"public","javascript",'facebooker.js')
+facebook_js = File.join(RAILS_ROOT,"public","javascripts",'facebooker.js')
 FileUtils.cp File.join(File.dirname(__FILE__) , 'facebooker.yml.tpl'), facebook_config unless File.exist?(facebook_config)
 FileUtils.cp File.join(File.dirname(__FILE__) , 'javascripts','facebooker.js'), facebook_js unless File.exist?(facebook_js)
 puts IO.read(File.join(File.dirname(__FILE__), 'README'))
