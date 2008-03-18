72d29eeb6528a2c60cbe01902c6a2ee4da695495
mmangino <mmangino@06148572-b36b-44fe-9aa8-f68b04d8b080>
2008-03-18 20:17:12 +0000

    remove ruby-debug
    git-svn-id: svn+ssh://rubyforge.org/var/svn/facebooker/trunk/facebooker@207 06148572-b36b-44fe-9aa8-f68b04d8b080

--- install.rb
+++ install.rb
@@ -1,6 +1,5 @@
 require 'fileutils'
 require 'rubygems'
-require 'ruby-debug'
 facebook_config = File.join(RAILS_ROOT,"config","facebooker.yml")
 facebook_js = File.join(RAILS_ROOT,"public","javascripts",'facebooker.js')
 FileUtils.cp File.join(File.dirname(__FILE__) , 'facebooker.yml.tpl'), facebook_config unless File.exist?(facebook_config)
