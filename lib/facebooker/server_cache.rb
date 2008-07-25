f80145efa4b79ea331e124c1aaa2b1c3653b5b8e
Michael Mangino <mmangino@elevatedrails.com>
2008-07-25 08:59:10 -0400

    Remove session from refs

--- lib/facebooker/server_cache.rb
+++ lib/facebooker/server_cache.rb
@@ -8,17 +8,17 @@ module Facebooker
     # Stores an FBML reference on the server for use 
     # across multiple users in FBML
     def set_ref_handle(handle_name, fbml_source)
-      (@session.post 'facebook.fbml.setRefHandle', :handle => handle_name, :fbml => fbml_source) == '1'
+      (@session.post 'facebook.fbml.setRefHandle', :handle => handle_name, :fbml => fbml_source,false) == '1'
     end
     
     ##
     # Fetches and re-caches the content stored at the given URL, for use in a fb:ref FBML tag.
     def refresh_ref_url(url)
-      (@session.post 'facebook.fbml.refreshRefUrl', :url => url) == '1'
+      (@session.post 'facebook.fbml.refreshRefUrl', :url => url,false) == '1'
     end
     
     def refresh_img_src(url)
-      (@session.post 'facebook.fbml.refreshImgSrc', :url => url) == '1'
+      (@session.post 'facebook.fbml.refreshImgSrc', :url => url,false) == '1'
     end
   end
 end
