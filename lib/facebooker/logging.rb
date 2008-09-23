7a5cfc931c31e6088a93909681a43a1ee2acc6b1
Michael Mangino <mmangino@elevatedrails.com>
2008-09-23 13:53:59 -0400

    Remove colorization of logging code and simplified code a little

--- lib/facebooker/logging.rb
+++ lib/facebooker/logging.rb
@@ -8,20 +8,6 @@ module Facebooker
   end
 
   module Logging
-    def self.refresh_colorize_logging
-      if Object.const_defined? :ActiveRecord
-        @@colorize = ActiveRecord::Base.colorize_logging
-      else
-        @@colorize = false
-      end
-    end
-    refresh_colorize_logging
-    
-    def self.colorize_logging
-      @@colorize
-    end
-    
-    @@row_even = false
     def self.log_fb_api(method, params)
       message = method # might customize later
       dump = format_fb_params(params)
@@ -35,40 +21,20 @@ module Facebooker
         nil
       end
     rescue Exception => e
-      # Log message and raise exception.
-      # Set last_verification to 0, so that connection gets verified
-      # upon reentering the request loop
       exception = "#{e.class.name}: #{e.message}: #{dump}"
       log_info(message, exception)
       raise
     end
     
     def self.format_fb_params(params)
-      params.to_a.map { |kvp| "#{kvp[0]} = #{kvp[1]}" }.join(', ')
+      params.map { |key,value| "#{key} = #{value}" }.join(', ')
     end
     
     def self.log_info(message, dump, seconds = 0)
       return unless Facebooker.logger
-      Facebooker.logger.debug format_log_entry("#{message} (#{'%f' % seconds})", dump)
+      log_message = "#{message} (#{seconds}) #{dump}"
+      Facebooker.logger.debug(log_message)
     end
     
-    # stolen from active record  
-    def self.format_log_entry(message, dump = nil)
-      if colorize_logging
-        if @@row_even
-          @@row_even = false
-          message_color, dump_color = "4;36;1", "0;1"
-        else
-          @@row_even = true
-          message_color, dump_color = "4;35;1", "0"
-        end
-  
-        log_entry = "  \e[#{message_color}m#{message}\e[0m   "
-        log_entry << "\e[#{dump_color}m%#{String === dump ? 's' : 'p'}\e[0m" % dump if dump
-        log_entry
-      else
-        "%s  %s" % [message, dump]
-      end
-    end    
   end  
 end
--- test/logging_test.rb
+++ test/logging_test.rb
@@ -14,51 +14,23 @@ class LoggingTest < Test::Unit::TestCase
   
   def test_does_not_crash_with_nil_logger
     Facebooker.logger = nil
-    Facebooker::Logging.refresh_colorize_logging
     Facebooker::Logging.log_fb_api('sample.api.call',
                           {'param1' => true, 'param2' => 'value2'})
   end
 
   def test_does_not_crash_outside_rails
-    Facebooker::Logging.refresh_colorize_logging
     flexmock(Facebooker.logger, :logger).should_receive(:debug).once.with(String)
     Facebooker::Logging.log_fb_api('sample.api.call',
                           {'param1' => true, 'param2' => 'value2'})
   end
 
   def test_plain_format
-    flexmock(ActiveRecord::Base, :colorize_logging => false)
-    Facebooker::Logging.refresh_colorize_logging
     flexmock(Facebooker.logger, :logger).should_receive(:debug).once.with(
-        'sample.api.call (0.000000)  param1 = true, param2 = value2')
+        'sample.api.call (0) param1 = true')
     Facebooker::Logging.log_fb_api('sample.api.call',
-                          {'param1' => true, 'param2' => 'value2'})
+                          {'param1' => true})
   ensure
     Facebooker.logger = nil
   end
 
-  def test_colorized_format
-    Facebooker::Logging.class_eval "@@row_even = false"
-    flexmock(ActiveRecord::Base, :colorize_logging => true)    
-    Facebooker::Logging.refresh_colorize_logging
-    flexmock(Facebooker.logger, :logger).should_receive(:debug).once.with(
-        "  \e[4;35;1msample.api.call (0.000000)\e[0m   \e[0mparam1 = true, param2 = value2\e[0m")
-    Facebooker::Logging.log_fb_api('sample.api.call',
-                          {'param1' => true, 'param2' => 'value2'})
-  end
-  
-  def test_colorized_exception
-    Facebooker::Logging.class_eval "@@row_even = false"
-    flexmock(ActiveRecord::Base, :colorize_logging => true)    
-    Facebooker::Logging.refresh_colorize_logging
-    flexmock(Facebooker.logger, :logger).should_receive(:debug).once.with(
-        "  \e[4;35;1msample.api.call (0.000000)\e[0m   \e[0mRuntimeError: Exception test: param1 = true, param2 = value2\e[0m")
-    
-    assert_raise RuntimeError do
-      Facebooker::Logging.log_fb_api('sample.api.call',
-                          {'param1' => true, 'param2' => 'value2'}) do
-        raise 'Exception test'
-      end
-    end   
-  end  
 end
