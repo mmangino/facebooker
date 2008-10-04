9b9ef9a9d874c04137b27c106f6bd696e00de435
Corey Innis <corey@coolerator.net>
2008-10-04 02:46:48 -0700

    initial experiment at getting generators to load from gem

---
+++ generators/facebooker/facebooker_generator.rb
@@ -0,0 +1,14 @@
+class FacebookerGenerator < Rails::Generator::Base
+  def manifest
+    record do |m|
+      m.file 'config/facebooker.yml.tmpl',       'config/facebooker.yml'
+      m.file 'public/javascripts/facebooker.js', 'public/javascripts/facebooker.js'
+    end
+  end
+
+  protected
+
+  def banner
+    "Usage: #{$0} facebooker"
+  end
+end
