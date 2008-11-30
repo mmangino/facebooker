bbd6d754b7d69061fb1693769c2eab537a550674
Stephen Wooten <quigebo468@gmail.com>
2008-11-30 16:26:58 -0600

    added additional permissions for the fb_prompt_permission helper

--- lib/facebooker/rails/helpers.rb
+++ lib/facebooker/rails/helpers.rb
@@ -283,7 +283,7 @@ module Facebooker
       VALID_FB_SHARED_PHOTO_SIZES = [:thumb, :small, :normal, :square]
       VALID_FB_PHOTO_SIZES = VALID_FB_SHARED_PHOTO_SIZES      
       VALID_FB_PROFILE_PIC_SIZES = VALID_FB_SHARED_PHOTO_SIZES
-      VALID_PERMISSIONS=[:email, :offline_access, :status_update, :photo_upload, :create_listing]
+      VALID_PERMISSIONS=[:email, :offline_access, :status_update, :photo_upload, :create_listing, :create_event, :rsvp_event, :sms]
       
       # Render an fb:tabs tag containing some number of fb:tab_item tags.
       # Example:
