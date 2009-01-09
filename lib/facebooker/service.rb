7048410b0832ffae6bc0253153a3a6b1b20e1c89
Michael Mangino <mmangino@elevatedrails.com>
2009-01-09 08:59:47 -0500

    Use curb if it is available

--- lib/facebooker/service.rb
+++ lib/facebooker/service.rb
@@ -1,4 +1,10 @@
-require 'net/http'
+begin
+  require 'curb'
+  Facebooker.use_curl = true
+rescue Exception=>e
+  puts e
+  require 'net/http'
+end
 require 'facebooker/parser'
 module Facebooker
   class Service
@@ -11,7 +17,7 @@ module Facebooker
     # TODO: support ssl 
     def post(params)
       attempt = 0
-      Parser.parse(params[:method], Net::HTTP.post_form(url, params))
+      Parser.parse(params[:method], post_form(url,params) )
     rescue Errno::ECONNRESET, EOFError
       if attempt == 0
         attempt += 1
@@ -19,13 +25,59 @@ module Facebooker
       end
     end
     
+    def post_form(url,params)
+      if Facebooker.use_curl?
+        post_form_with_curl(url,params)
+      else
+        post_form_with_net_http(url,params)
+      end
+    end
+    
+    def post_form_with_net_http(url,params)
+      Net::HTTP.post_form(url, params)
+    end
+    
+    def post_form_with_curl(url,params,multipart=false)
+      response = Curl::Easy.http_post(url.to_s, *to_curb_params(params)) do |c|
+        c.multipart_form_post = multipart
+        c.timeout = Facebooker.timeout 
+      end
+      response.body_str
+    end
+    
+    def post_multipart_form(url,params)
+      if Facebooker.use_curl?
+        post_form_with_curl(url,params,true)
+      else
+        post_multipart_form_with_net_http(url,params)
+      end
+    end
+    
+    def post_multipart_form_with_net_http(url,params)
+      Net::HTTP.post_multipart_form(url, params)
+    end
+    
     def post_file(params)
-      Parser.parse(params[:method], Net::HTTP.post_multipart_form(url, params))
+      Parser.parse(params[:method], post_multipart_form(url, params))
     end
     
     private
     def url
       URI.parse('http://'+ @api_base + @api_path)
+    end
+    
+    def multipart_post_file?(object)
+      object.respond_to?(:content_type) &&
+      object.respond_to?(:data) &&
+      object.respond_to?(:filename)
+    end
+    
+    def to_curb_params(params)
+      parray = []
+      params.each_pair do |k,v|
+        parray << (multipart_post_file?(v) ? Curl::PostField.file((k.nil? ? nil : k.to_s), v.filename.to_s) : Curl::PostField.content(k.to_s, v.to_s).to_s)
+      end
+      parray
     end
   end
 end
