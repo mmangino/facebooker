66838cf9cf890be7348bf86f0e6ebf01a33ab744
Daniel Morrison <daniel@collectiveidea.com>
2009-03-02 17:58:39 -0500

    Refactored pretty errors to be more easily overridden.

--- lib/facebooker/rails/facebook_pretty_errors.rb
+++ lib/facebooker/rails/facebook_pretty_errors.rb
@@ -1,14 +1,12 @@
-if Facebooker.facebooker_config['pretty_errors'] || (Facebooker.facebooker_config['pretty_errors'].nil? && RAILS_ENV=="development")
-  class ActionController::Base
-    def rescues_path_with_facebooker(template_name)
-      t="#{RAILS_ROOT}/vendor/plugins/facebooker/templates/#{template_name}.erb"
-      File.exist?(t) ? t : rescues_path_without_facebooker(template_name)
-    end
+class ActionController::Base
+  def rescues_path_with_facebooker(template_name)
+    t="#{RAILS_ROOT}/vendor/plugins/facebooker/templates/#{template_name}.erb"
+    File.exist?(t) ? t : rescues_path_without_facebooker(template_name)
+  end
 
-    alias_method_chain :rescues_path,:facebooker
+  alias_method_chain :rescues_path, :facebooker if Facebooker.facebooker_config['pretty_errors'] || (Facebooker.facebooker_config['pretty_errors'].nil? && RAILS_ENV=="development")
 
-    def response_code_for_rescue(exception)
-      200
-    end
+  def response_code_for_rescue(exception)
+    200
   end
 end
