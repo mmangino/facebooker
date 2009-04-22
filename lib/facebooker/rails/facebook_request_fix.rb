a758a6fed3b2d6e10466b73ef3c7ce36414bdd4d
Erkki Eilonen <erkki@itech.ee>
2009-04-22 16:22:08 +0300

    respond correctly to facebook HEAD requests

--- lib/facebooker/rails/facebook_request_fix.rb
+++ lib/facebooker/rails/facebook_request_fix.rb
@@ -1,8 +1,10 @@
 module ::ActionController
   class AbstractRequest
     def request_method_with_facebooker
-      if parameters[:fb_sig_request_method]=="GET" and parameters[:_method].blank?
-        parameters[:_method]="GET"
+      if parameters[:_method].blank?
+        if %w{GET HEAD}.include?(parameters[:fb_sig_request_method])
+          parameters[:_method] = parameters[:fb_sig_request_method]
+        end
       end
       request_method_without_facebooker
     end
