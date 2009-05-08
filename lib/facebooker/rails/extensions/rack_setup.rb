8b2500f8ecc99fdfafb7e2542cf022ac633347a5
Matthew Beale <mixonic@synitech.com>
2009-05-08 10:08:39 -0400

    Allow the rack middle_ware to fall back from after RewindableInput to after Session::CookieStore if RewindableInput is not available.  It was removed somewhere in Rails 2.3

--- lib/facebooker/rails/extensions/rack_setup.rb
+++ lib/facebooker/rails/extensions/rack_setup.rb
@@ -1,2 +1,8 @@
+# Somewhere in 2.3 RewindableInput was removed- rack supports it natively
 require 'rack/facebook'
-ActionController::Dispatcher.middleware.insert_after 'ActionController::RewindableInput',Rack::Facebook, Facebooker.secret_key
+ActionController::Dispatcher.middleware.insert_after( 
+  (Object.const_get('ActionController::RewindableInput') rescue false) ?
+    'ActionController::RewindableInput' :
+    'ActionController::Session::CookieStore',
+  Rack::Facebook,
+  Facebooker.secret_key )
