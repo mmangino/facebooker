e6def15381ce9fe22e80ec9560d01115f5de8de7
Matthew Beale <mixonic@synitech.com>
2009-05-15 07:24:58 -0400

    Abstract the Rail param checks out into a module

--- lib/facebooker/rails/facebook_request_fix.rb
+++ lib/facebooker/rails/facebook_request_fix.rb
@@ -1,5 +1,7 @@
 module ::ActionController
   class AbstractRequest
+    include Facebooker::Rails::BackwardsCompatibleParamChecks
+
     def request_method_with_facebooker
       if parameters[:_method].blank?
         if %w{GET HEAD}.include?(parameters[:fb_sig_request_method])
@@ -14,8 +16,8 @@ module ::ActionController
     end
 
     def xml_http_request_with_facebooker?
-      parameters["fb_sig_is_mockajax"] == "1"  ||
-      parameters["fb_sig_is_ajax"] == "1" ||
+      one_or_true(parameters["fb_sig_is_mockajax"])  ||
+      one_or_true(parameters["fb_sig_is_ajax"]) ||
       xml_http_request_without_facebooker?
     end
     alias_method_chain :xml_http_request?, :facebooker
