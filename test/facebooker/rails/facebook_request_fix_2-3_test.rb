b5b9289af4f5fb72a9e4a3140e02100ad16b4859
Joey Aghion <joey@givereal.com>
2009-05-18 13:31:56 -0400

    update request#xml_http_request? and #xhr? to work with Rails 2.3

---
+++ test/facebooker/rails/facebook_request_fix_2-3_test.rb
@@ -0,0 +1,24 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../rails_test_helper')
+
+class Facebooker::Rails::FacebookRequestFix23Test < Test::Unit::TestCase
+
+  def setup
+    ENV['FACEBOOK_API_KEY'] = '1234567'
+    ENV['FACEBOOK_SECRET_KEY'] = '7654321'
+    if Rails.version < '2.3'
+      @request = ActionController::TestRequest.new({"fb_sig_is_ajax"=>"1"}, {}, nil)
+    else
+      @request = ActionController::TestRequest.new
+      @request.query_parameters[:fb_sig_is_ajax] = "1"
+    end
+  end
+
+  def test_xhr_when_is_ajax
+    assert @request.xhr?
+  end
+
+  def test_xml_http_request_when_is_ajax
+    assert @request.xml_http_request?
+  end
+
+end
