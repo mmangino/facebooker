22cceb89ff0aef936c63cc891178dc7b2a53490d
Mike Mangino <mmangino@elevatedrails.com>
2009-05-21 12:02:06 -0400

    Improved support for Rails 2.3.2

--- lib/facebooker/rails/controller.rb
+++ lib/facebooker/rails/controller.rb
@@ -207,7 +207,9 @@ module Facebooker
       
       def verify_signature(facebook_sig_params,expected_signature)
         # Don't verify the signature if rack has already done so.
-        return if ActionController::Dispatcher.middleware.include? Rack::Facebook
+        if ::Rails.version >= "2.3"
+          return if ActionController::Dispatcher.middleware.include? Rack::Facebook
+        end
         raw_string = facebook_sig_params.map{ |*args| args.join('=') }.sort.join
         actual_sig = Digest::MD5.hexdigest([raw_string, Facebooker::Session.secret_key].join)
         raise Facebooker::Session::IncorrectSignature if actual_sig != expected_signature
