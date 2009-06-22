9493b8e7aebca98072e41fada8837598d7a50599
Joey Aghion <joey@aghion.com>
2009-06-22 10:54:04 -0400

    consider blank to be zero_or_false for backwards-compatible param-checking

--- test/facebooker/rails/facebook_url_rewriting_test.rb
+++ test/facebooker/rails/facebook_url_rewriting_test.rb
@@ -11,7 +11,7 @@ class Facebooker::Rails::FacebookUrlRewritingTest < Test::Unit::TestCase
   def test_one_or_true_on_string_1
     assert @rewriter.one_or_true( "1" )
   end
-  
+
   def test_one_or_true_on_string_0
     assert !@rewriter.one_or_true( "0" )
   end
@@ -23,7 +23,7 @@ class Facebooker::Rails::FacebookUrlRewritingTest < Test::Unit::TestCase
   def test_one_or_true_on_float_1
     assert @rewriter.one_or_true( 1.0 )
   end
-  
+
   def test_one_or_true_on_true
     assert @rewriter.one_or_true( true )
   end
@@ -34,6 +34,43 @@ class Facebooker::Rails::FacebookUrlRewritingTest < Test::Unit::TestCase
 
   def test_one_or_true_on_nil
     assert !@rewriter.one_or_true( nil )
+  end
+
+
+  def test_zero_or_false_on_blank
+    assert @rewriter.zero_or_false( "" )
+  end
+
+  def test_zero_or_false_on_integer_0
+    assert @rewriter.zero_or_false( 0 )
+  end
+
+  def test_zero_or_false_on_float_0
+    assert @rewriter.zero_or_false( 0.0 )
+  end
+
+  def test_zero_or_false_on_string_0
+    assert @rewriter.zero_or_false( "0" )
+  end
+
+  def test_zero_or_false_on_false
+    assert @rewriter.zero_or_false( false )
+  end
+
+  def test_zero_or_false_on_nil
+    assert @rewriter.zero_or_false( nil )
+  end
+
+  def test_zero_or_false_on_string_1
+    assert !@rewriter.zero_or_false( "1" )
+  end
+
+  def test_zero_or_false_on_numeric_1
+    assert !@rewriter.zero_or_false( 1 )
+  end
+
+  def test_zero_or_false_on_true
+    assert !@rewriter.zero_or_false( true )
   end
 
 end
