5597a484277deb81e80e65d7d8f34d7056daeee1
Pavel Kunc <pavel.kunc@gmail.com>
2009-08-06 19:06:46 +0100

    Fix Facebooker::Service::BaseService#post_params for Ruby 1.9.1 by making sure that keys are not Symbols.

--- lib/facebooker/service/base_service.rb
+++ lib/facebooker/service/base_service.rb
@@ -6,6 +6,7 @@ class Facebooker::Service::BaseService
   def post_params(params)
     post_params = {}
     params.each do |k,v|
+      k = k.to_s unless k.is_a?(String)
       if Array === v || Hash === v
         post_params[k] = Facebooker.json_encode(v)       
       else
