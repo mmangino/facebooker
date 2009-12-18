168f54f5baec1edc3276e458e9c90e23f0537e1e
W. Andrew Loe III <andrew@andrewloe.com>
2009-12-17 17:25:44 -0800

    Rack Middleware to convert the fb_sig_session_key to the Rails session key.

--- lib/facebooker/rails/extensions/rack_setup.rb
+++ lib/facebooker/rails/extensions/rack_setup.rb
@@ -1,6 +1,14 @@
 # Somewhere in 2.3 RewindableInput was removed- rack supports it natively
 require 'rack/facebook'
+require 'rack/facebook_session'
+
 ActionController::Dispatcher.middleware.insert_before( 
   ActionController::ParamsParser,
   Rack::Facebook
+)
+
+ActionController::Dispatcher.middleware.insert_before(
+  ActionController::Base.session_store,
+  Rack::FacebookSession,
+  ActionController::Base.session_options[:key]
 )
