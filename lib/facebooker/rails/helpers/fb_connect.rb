26689d50c49637f864ec44fe4ef035d3b8b6d767
Jacob Dunphy <jacob.dunphy@gmail.com>
2010-02-26 16:52:31 -0800

    Allow a :text option to end up as the fb:login-button value for login button helper methods.

--- lib/facebooker/rails/helpers/fb_connect.rb
+++ lib/facebooker/rails/helpers/fb_connect.rb
@@ -65,37 +65,64 @@ module Facebooker
           end
         end
 
+        #
         # Render an <fb:login-button> element
         #
         # ==== Examples
         #
-        # <%= fb_login_button%>
-        # => <fb:login-button></fb:login-button>
+        #   <%= fb_login_button%>
+        #   => <fb:login-button></fb:login-button>
         #
         # Specifying a javascript callback
         #
-        # <%= fb_login_button 'update_something();'%>
-        # => <fb:login-button onlogin='update_something();'></fb:login-button>
+        #   <%= fb_login_button 'update_something();'%>
+        #   => <fb:login-button onlogin='update_something();'></fb:login-button>
         #
         # Adding options <em>See:</em> http://wiki.developers.facebook.com/index.php/Fb:login-button
         #
-        # <%= fb_login_button 'update_something();', :size => :small, :background => :dark%>
-        # => <fb:login-button background='dark' onlogin='update_something();' size='small'></fb:login-button>
+        #   <%= fb_login_button 'update_something();', :size => :small, :background => :dark%>
+        #   => <fb:login-button background='dark' onlogin='update_something();' size='small'></fb:login-button>
         #
+        # :text option allows you to set the text value of the
+        # button.  *A note!* This will only do what you expect it to do
+        # if you set :v => 2 as well.
+        #
+        #   <%= fb_login_button 'update_somethign();',
+        #        :text => 'Loginto Facebook', :v => 2 %>
+        #   => <fb:login-button v='2' onlogin='update_something();'>Login to Facebook</fb:login-button>
         def fb_login_button(*args)
 
           callback = args.first
           options = args[1] || {}
-          options.merge!(:onlogin=>callback)if callback
+          options.merge!(:onlogin=>callback) if callback
 
-          content_tag("fb:login-button",nil, options)
+          text = options.delete(:text)
+
+          content_tag("fb:login-button",text, options)
         end
 
+        #
+        # Render an <fb:login-button> element, similar to
+        # fb_login_button. Adds a js redirect to the onlogin event via rjs.
+        #
+        # ==== Examples
+        #
+        #   fb_login_and_redirect '/other_page'
+        #   => <fb:login-button onlogin="window.location.href = &quot;/other_page&quot;;"></fb:login-button>
+        #
+        # Like #fb_login_button, this also supports the :text option
+        #
+        #   fb_login_and_redirect '/other_page', :text => "Login with Facebook", :v => '2'
+        #   => <fb:login-button onlogin="window.location.href = &quot;/other_page&quot;;" v="2">Login with Facebook</fb:login-button>
+        #
         def fb_login_and_redirect(url, options = {})
           js = update_page do |page|
             page.redirect_to url
           end
-          content_tag("fb:login-button",nil,options.merge(:onlogin=>js))
+
+          text = options.delete(:text)
+          
+          content_tag("fb:login-button",text,options.merge(:onlogin=>js))
         end
 
         def fb_unconnected_friends_count
