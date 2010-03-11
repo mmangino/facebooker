8cbb6bbb5190ac6bb8c420bf047700c767cb9882
Brian Ross <brian@bridev.com>
2010-03-11 12:44:40 -0700

    Added attr_accessor for :uid and :source to Status class inside user model.

--- lib/facebooker/models/user.rb
+++ lib/facebooker/models/user.rb
@@ -9,7 +9,7 @@ module Facebooker
     include Model
     class Status
       include Model
-      attr_accessor :message, :time, :status_id
+      attr_accessor :uid, :message, :time, :status_id, :source
     end
     FIELDS = [:status, :political, :pic_small, :name, :quotes, :is_app_user, :tv, :profile_update_time, :meeting_sex, :hs_info, :timezone, :relationship_status, :hometown_location, :about_me, :wall_count, :significant_other_id, :pic_big, :music, :work_history, :sex, :religion, :notes_count, :activities, :pic_square, :movies, :has_added_app, :education_history, :birthday, :birthday_date, :first_name, :meeting_for, :last_name, :interests, :current_location, :pic, :books, :affiliations, :locale, :profile_url, :proxied_email, :email_hashes, :allowed_restrictions, :pic_with_logo, :pic_big_with_logo, :pic_small_with_logo, :pic_square_with_logo, :online_presence, :verified, :profile_blurb, :username, :website, :is_blocked, :family, :email]
     STANDARD_FIELDS = [:uid, :first_name, :last_name, :name, :timezone, :birthday, :sex, :affiliations, :locale, :profile_url, :proxied_email, :email]
