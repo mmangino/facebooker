1349e90bf2deb6adc8e8526dda2136116216feaf
Erik Ogan <erik@contextoptional.com>
2010-04-14 17:06:16 -0700

    Support for method 'facebook.events.invite'

--- lib/facebooker/parser.rb
+++ lib/facebooker/parser.rb
@@ -611,6 +611,12 @@ module Facebooker
       end
     end
 
+    class EventsInvite < Parser#:nodoc:
+        def self.process(data)
+          booleanize(element('events_invite_response', data).content)
+        end
+      end
+
   class GroupGetMembers < Parser#:nodoc:
     def self.process(data)
       root = element('groups_getMembers_response', data)
@@ -949,6 +955,7 @@ module Facebooker
       'facebook.events.cancel' => EventsCancel,
       'facebook.events.get' => EventsGet,
       'facebook.events.edit' => EventsEdit,
+      'facebook.events.invite' => EventsInvite,
       'facebook.events.rsvp' => EventsRsvp,
       'facebook.groups.get' => GroupsGet,
       'facebook.events.getMembers' => EventMembersGet,
