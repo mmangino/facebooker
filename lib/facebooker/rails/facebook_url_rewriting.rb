f134d12d899c5f4f092327a7f5178dd1becbb615
Mike Mangino <mmangino@elevatedrails.com>
2010-06-23 07:49:50 -0400

    Fix for older versions of rails

--- lib/facebooker/rails/facebook_url_rewriting.rb
+++ lib/facebooker/rails/facebook_url_rewriting.rb
@@ -44,7 +44,11 @@ module ::ActionController
   
     def rewrite_url_with_facebooker(*args)
       options = args.first.is_a?(Hash) ? args.first : args.last
-      is_link_to_canvas = @request.env["REQUEST_METHOD"] == "POST" && link_to_canvas?(@request.request_parameters, options)
+      if Rails.version >= '2.3.8'
+        is_link_to_canvas = @request.env["REQUEST_METHOD"] == "POST" && link_to_canvas?(@request.request_parameters, options)
+      else
+        is_link_to_canvas = link_to_canvas?(@request.request_parameters, options)
+      end
       if is_link_to_canvas && !options.has_key?(:host)
         options[:host] = Facebooker.canvas_server_base
       end 
